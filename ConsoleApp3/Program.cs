2d65651b9fc699e7e775adf34d938e88db1d399f
aanchal-subedi <aanchalsubedi005@gmail.com>
2023-04-27 13:05:21 +0545

    vdg

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Reflection.Metadata;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("ora", "oracle");
+        ht.Add("vb", "vb.NET");
+        foreach (DictionaryEntry d in ht)
+        {
+            Console.WriteLine(d.Key + " :" + d.Value + " ");
+        }
+        Console.ReadKey();
+    }
+}
